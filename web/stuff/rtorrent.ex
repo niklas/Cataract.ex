c0505f400d1bb2e230cd460f37557dfce138475d
Niklas Hofer <niklas+dev@lanpartei.de>
2015-12-28 19:41:56 +0100

    we can send and receive something

--- web/stuff/rtorrent.ex
+++ web/stuff/rtorrent.ex
@@ -3,10 +3,24 @@ defmodule Cataract.Rtorrent do
     url = '/home/niklas/rails/cataract/tmp/sockets/rtorrent_test'
 
     case :afunix.connect(url, []) do
-      {:ok, _socket} ->
+      {:ok, socket} ->
         IO.puts "connected"
+        ping socket
       {:error, error} ->
         IO.puts error
+    end
+  end
+
+  def ping(socket) do
+    data = '12345678'
+    :ok = :afunix.send(socket, data)
+    case :afunix.recv(socket, 1000, 10) do
+      {:ok, resp} ->
+        IO.puts "response:"
+        IO.puts resp
+      {:error, err} ->
+        IO.puts "error"
+        IO.puts err
     end
   end
 end
