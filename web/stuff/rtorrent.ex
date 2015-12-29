6b84f4df32a0339006a302056063d1dbf5ecba30
Niklas Hofer <niklas+dev@lanpartei.de>
2015-12-29 03:00:56 +0100

    we get the full response \o/

--- web/stuff/rtorrent.ex
+++ web/stuff/rtorrent.ex
@@ -5,34 +5,38 @@ defmodule Cataract.Rtorrent do
     case :afunix.connect(url, []) do
       {:ok, socket} ->
         IO.puts "connected"
-        ping socket
+        null = "\0"
+        data = "64:" <>
+        "CONTENT_LENGTH" <> null <> "100" <> null <>
+        "SCGI" <> null <> "1" <> null <>
+        "REQUEST_METHOD" <> null <> "POST" <> null <>
+        "REQUEST_URI" <> null <> "/RPC2" <> null <>
+        ",<?xml version=\"1.0\" ?><methodCall><methodName>system.listMethods</methodName><params/></methodCall>" <> "\n"
+        :ok = :afunix.send(socket, data)
+        collect_response([])
       {:error, error} ->
         IO.puts error
     end
   end
 
-  def ping(socket) do
-    null = "\0"
-    data = "64:" <>
-    "CONTENT_LENGTH" <> null <> "100" <> null <>
-    "SCGI" <> null <> "1" <> null <>
-    "REQUEST_METHOD" <> null <> "POST" <> null <>
-    "REQUEST_URI" <> null <> "/RPC2" <> null <>
-    ",<?xml version=\"1.0\" ?><methodCall><methodName>system.listMethods</methodName><params/></methodCall>" <> "\n"
-    :ok = :afunix.send(socket, data)
+  def collect_response(xml) do
     receive do
       {:tcp, _s, resp} ->
-        IO.puts "response:"
-        IO.puts resp
-      {:tcp_error, err} ->
+        IO.puts "response"
+        collect_response( [xml, resp] )
+      {:tcp_error, _s, err} ->
         IO.puts err
+      {:tcp_closed, socket} ->
+        IO.puts to_string(xml)
+        IO.puts "done."
+        :ok = :afunix.close(socket)
       {a,b,c} ->
+        IO.puts "WTF:"
         IO.puts a
         IO.puts b
         IO.puts a
       Else ->
         IO.puts ".:"
     end
-    :ok = :afunix.close(socket)
   end
 end
