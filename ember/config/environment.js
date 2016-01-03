1b067846d011e030f91194ece3f49c3de878d90c
Niklas Hofer <niklas+dev@lanpartei.de>
2016-01-03 01:56:27 +0100

    minimal list of all transfers from JSON api

--- ember/config/environment.js
+++ ember/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'cataract',
     environment: environment,
     baseURL: '/ember',
-    locationType: 'none',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
