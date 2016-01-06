ea87d1a26093d760f7c376b8b78925a37d9c977a
Niklas Hofer <niklas+dev@lanpartei.de>
2016-01-06 22:11:41 +0100

    just forward the error from rtorrent

--- web/controllers/transfer_controller.ex
+++ web/controllers/transfer_controller.ex
@@ -2,7 +2,13 @@ defmodule Cataract.TransferController do
   use Cataract.Web, :controller
 
   def index(conn, _p) do
-    {:ok, transfers } = Cataract.Rtorrent.find_all
-    render conn, transfers: transfers
+     case  Cataract.Rtorrent.find_all do
+       {:ok, transfers } ->
+         render conn, transfers: transfers
+       { :error, error } ->
+         conn
+         |> put_status(503)
+         |> text(error)
+     end
   end
 end
