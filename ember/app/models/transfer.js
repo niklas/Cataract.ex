556de5464928c373382430484eebb70c877e4bb5
Niklas Hofer <niklas+dev@lanpartei.de>
2016-01-09 21:54:22 +0100

    up/down colored indicators

--- ember/app/models/transfer.js
+++ ember/app/models/transfer.js
@@ -5,5 +5,9 @@ export default DS.Model.extend({
   completedBytes: DS.attr('number'),
   ratio: DS.attr('number'),
   upRate: DS.attr('number'),
-  downRate: DS.attr('number')
+  downRate: DS.attr('number'),
+
+  percentCompleted: function() {
+      return 100 * ( this.get('completedBytes') / this.get('sizeBytes') )
+  }.property('sizeBytes', 'completedBytes'),
 });
