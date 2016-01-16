41b248c11a73376c8c50bfcd255d0def189bfcb5
Niklas Hofer <niklas+dev@lanpartei.de>
2016-01-16 23:29:56 +0100

    bump to 0.3.1 for code reload testing

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cataract.Mixfile do
 
   def project do
     [app: :cataract,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
