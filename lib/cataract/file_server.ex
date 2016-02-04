0458d2f8124b0da1886c92de80490c21d8f06d51
Niklas Hofer <niklas+dev@lanpartei.de>
2016-02-04 23:32:52 +0100

    use mlocate to index and find files

--- lib/cataract/file_server.ex
+++ lib/cataract/file_server.ex
@@ -1,8 +1,25 @@
 defmodule Cataract.FileServer do
-  def start_link do
+  def start_link(root) do
+    db = "/tmp/cataract-fileserver.db"
+    System.cmd "updatedb", [
+      "--database-root", root,
+      "--output", db,
+      "--require-visibility", "no"
+    ]
   end
 
   def find_file(filename) do
-    ["test/cataract/file_server_test.exs"]
+    db = "/tmp/cataract-fileserver.db"
+    case System.cmd "locate", [
+      "--database", db,
+      "--basename",
+      "--regexp",
+      filename <> "$"
+    ] do
+      { out, 0 } ->
+        String.split(out, "\n")
+      { _, 1 } ->
+        :nothing_found
+    end
   end
 end
--- test/cataract/file_server_test.exs
+++ test/cataract/file_server_test.exs
@@ -2,7 +2,7 @@ defmodule Cataract.FileServerTest do
   use ExUnit.Case
 
   test "finding an existing file by exact name" do
-    Cataract.FileServer.start_link
+    Cataract.FileServer.start_link(".")
 
     assert Cataract.FileServer.find_file("file_server_test.exs") == ["test/cataract/file_server_test.exs"]
   end
