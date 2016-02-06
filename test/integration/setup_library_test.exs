7114d0ca661f6e95d2e241234bbcf43d99aa6f9b
Niklas Hofer <niklas+dev@lanpartei.de>
2016-02-06 20:35:45 +0100

    TODO: add disk => torrents imported

--- test/integration/setup_library_test.exs
+++ test/integration/setup_library_test.exs
@@ -8,9 +8,20 @@ defmodule Cataract.SetupLibraryTest do
   hound_session
 
   test "setup disks, finds torrents" do
+    fs_root = Path.absname("./") <> "/tmp/test_fs"
+
     navigate_to "/ember"
     click({:link_text, "Library"})
     assert visible_in_page?(~r/Disks/)
-    # TODO: click "Setup"
+    click({:link_text, "Register Disk"})
+    fill_field({:name, "path"}, fs_root)
+    submit_element({:link_text, "Register"})
+
+    assert visible_in_page?(~r/test_fs/)
+    # wait for the indexers to run
+    :timer.sleep(1000)
+    assert visible_in_page?(~r/Incoming/), "Imports and shows Directory"
+    assert visible_in_page?(~r/Torrents: 2/), "Imports and show number of torrents"
+    assert visible_in_page?(~r/Accounted Space: 123kByte/), "Show how much used space is accounted for"
   end
 end
