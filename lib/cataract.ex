40998b078e344147b3e8e4cb9979bdbd4992aa65
Niklas Hofer <niklas+dev@lanpartei.de>
2016-02-07 02:10:55 +0100

    index disk asynchronously

--- lib/cataract.ex
+++ lib/cataract.ex
@@ -13,6 +13,7 @@ defmodule Cataract do
       supervisor(Cataract.Repo, []),
       # Here you could define other workers and supervisors as children
       # worker(Cataract.Worker, [arg1, arg2, arg3]),
+      worker(Cataract.Library, []),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
--- lib/cataract/library.ex
+++ lib/cataract/library.ex
@@ -1,4 +1,20 @@
+require Logger
 defmodule Cataract.Library do
-  def index disk = %Cataract.Disk{} do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: {:global, "Lib"})
+  end
+
+  def index thingy  do
+    GenServer.cast {:global, "Lib"}, {:index, thingy }
+  end
+
+
+  ### GenServer API
+
+  def handle_cast({:index, %Cataract.Disk{} = disk}, status) do
+    Logger.debug("########## Indexing disk #{disk.path}")
+    {:noreply, status}
   end
 end
