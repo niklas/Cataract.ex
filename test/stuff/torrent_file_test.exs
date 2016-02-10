1fdc5afe36bd4490c3adfd94aa0b5bfdd26e9993
Niklas Hofer <niklas+dev@lanpartei.de>
2016-02-10 20:34:14 +0100

    can check if payload exists completely

--- test/stuff/torrent_file_test.exs
+++ test/stuff/torrent_file_test.exs
@@ -9,11 +9,45 @@ defmodule Cataract.TorrentFileTest do
     assert TorrentFile.meta_from_file(fs_src <> "/Incoming/multiple.torrent") == %{
       size_bytes: 73451+128,
       info_hash: "594EF81CC61C94C1BCDB2CAFCF1E72B635DFF1B5",
+      files: [
+        %{ "path" => ["banane.poem"], "length" => 128 },
+        %{ "path" => ["tails.png"],   "length" => 73451 },
+      ],
     }
 
     assert TorrentFile.meta_from_file(fs_src <> "/Incoming/single.torrent") == %{
       size_bytes: 73451,
       info_hash: "AE596A610B269617E9163E69F82AFDACE20324DD",
+      files: [
+        %{ "path" => ["tails.png"],   "length" => 73451 },
+      ],
     }
+  end
+
+  test "provides existance of files on disk" do
+    fs_src  = Path.absname("./") <> "/test/fixtures/fs"
+    fs_root = Path.absname("./") <> "/tmp/test_fs"
+
+    torrent = fs_root <> "/Incoming/multiple.torrent"
+    payload = fs_root <> "/Incoming/banane.poem"
+
+    { _, 0 } = System.cmd "rsync", [
+      "-a", "--delete",
+      fs_src <> "/",
+      fs_root <> "/",
+    ]
+
+    assert TorrentFile.payload_exists?( torrent, fs_root <> "/Incoming")
+    # again
+    assert TorrentFile.payload_exists?( torrent, fs_root <> "/Incoming")
+    refute TorrentFile.payload_exists?( torrent, fs_root)
+
+    # change the file size of one of the payload files
+      File.open(payload, [:write, :append], fn (file) ->
+        IO.write(file, "The End")
+      end)
+
+    # one file changed => not considered existing
+    refute TorrentFile.payload_exists?( torrent, fs_root <> "/Incoming")
   end
 end
--- web/stuff/torrent_file.ex
+++ web/stuff/torrent_file.ex
@@ -1,20 +1,54 @@
 defmodule Cataract.TorrentFile do
   def meta_from_file(file) do
-    case file
-      |> File.read!
-      |> Bencode.decode do
+    case decode_file(file) do
         # single file
-        {:ok, %{"info" => info = %{"length" => size} } } ->
-          %{size_bytes: size, info_hash: hash_from_info(info)}
+        {:ok, %{"info" => info = %{"length" => size, "name" => name} } } ->
+          files = []
+          %{
+            size_bytes: size,
+            info_hash:  hash_from_info(info),
+            files:      [%{"length" => size, "path" => [name]}],
+          }
         # multifile
         {:ok, %{"info" => info = %{"files" => files} } } ->
           size = files
             |> Enum.map(fn (f)-> f["length"] end)
             |> Enum.sum
-          %{size_bytes: size, info_hash: hash_from_info(info)}
+          %{
+            size_bytes: size,
+            info_hash:  hash_from_info(info),
+            files:      files,
+          }
         {:error, e } ->
           { :error, e }
       end
+  end
+
+  def payload_exists?(torrent_path, payload_path) do
+    case meta_from_file(torrent_path) do
+      %{files: files} ->
+        files
+          |> Enum.map( fn (%{"path" => f, "length" => s})->
+            { Path.join(payload_path, f), s }
+          end)
+          |> Enum.all?( fn ({p,size})->
+            case File.stat(p) do
+              {:ok, %File.Stat{size: size_on_disk}} ->
+                size_on_disk == size
+              {:error, _ } ->
+                false
+            end
+          end)
+      _ -> false
+    end
+  end
+
+  ### Utilities
+
+  def decode_file(file) do
+    file
+      |> File.read!
+      |> Bencode.decode
   end
 
   def hash_from_info(info) do
