5c85f5ee00cfb9b39728b751bc927dec4afe500b
Niklas Hofer <niklas+dev@lanpartei.de>
2016-02-10 04:44:08 +0100

    calculate info_hash

--- test/stuff/torrent_file_test.exs
+++ test/stuff/torrent_file_test.exs
@@ -7,11 +7,13 @@ defmodule Cataract.TorrentFileTest do
     fs_src  = Path.absname("./") <> "/test/fixtures/fs"
 
     assert TorrentFile.meta_from_file(fs_src <> "/Incoming/multiple.torrent") == %{
-      size_bytes: 73451+128
+      size_bytes: 73451+128,
+      info_hash: "594EF81CC61C94C1BCDB2CAFCF1E72B635DFF1B5",
     }
 
     assert TorrentFile.meta_from_file(fs_src <> "/Incoming/single.torrent") == %{
-      size_bytes: 73451
+      size_bytes: 73451,
+      info_hash: "AE596A610B269617E9163E69F82AFDACE20324DD",
     }
   end
 end
--- web/stuff/torrent_file.ex
+++ web/stuff/torrent_file.ex
@@ -4,16 +4,21 @@ defmodule Cataract.TorrentFile do
       |> File.read!
       |> Bencode.decode do
         # single file
-        {:ok, %{"info" => %{"length" => size} } } ->
-          %{size_bytes: size}
+        {:ok, %{"info" => info = %{"length" => size} } } ->
+          %{size_bytes: size, info_hash: hash_from_info(info)}
         # multifile
-        {:ok, %{"info" => %{"files" => files} } } ->
+        {:ok, %{"info" => info = %{"files" => files} } } ->
           size = files
             |> Enum.map(fn (f)-> f["length"] end)
             |> Enum.sum
-          %{size_bytes: size}
+          %{size_bytes: size, info_hash: hash_from_info(info)}
         {:error, e } ->
           { :error, e }
       end
+  end
+
+  def hash_from_info(info) do
+    :crypto.hash(:sha, Bencode.encode!(info))
+      |> Base.encode16
   end
 end
